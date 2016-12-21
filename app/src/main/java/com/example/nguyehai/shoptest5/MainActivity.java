658e912be5422f69e10e690ab57e7bd0624e221c
bkitnhai <bkit.n.hai@gmail.com>
2016-12-21 11:40:04 +0100

    this is 5 nd change from mini

--- app/src/main/java/com/example/nguyehai/shoptest5/MainActivity.java
+++ app/src/main/java/com/example/nguyehai/shoptest5/MainActivity.java
@@ -8,7 +8,7 @@ package com.example.nguyehai.shoptest5;
 public class MainActivity extends TabActivity {
 
     // test 3
-    //this is from main pc daffasdf
+    //im using this one from mini
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
